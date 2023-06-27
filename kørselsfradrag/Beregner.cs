fb2103882d5954f2df06aa112b470f5fe9837f67
hazavi <sanchaiavila@gmail.com>
2023-06-27 07:42:03 +0200

    Add project files.

---
+++ kørselsfradrag/Beregner.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using kørselsfradrag;
+
+namespace kørselsfradrag
+{
+    public class Beregner
+    {
+        public void Start()
+        {
+            Console.Clear();
+            Console.Write("Indtast antal kilometer: ");
+            string input = Console.ReadLine();
+            int bropenge = Bro();
+
+            double antalKilometer;
+            if (double.TryParse(input, out antalKilometer))
+            {
+                KilometerBeregner kilometerBeregner = new KilometerBeregner();
+                kilometerBeregner.AntalKilometer = antalKilometer;
+                double fradrag = kilometerBeregner.BeregnPris();
+                fradrag = Math.Round(fradrag + bropenge, 2);
+
+                Console.WriteLine($"Fradrag for {kilometerBeregner.AntalKilometer} kilometer er: {fradrag.ToString("N2")} kr.");
+            }
+            else
+            {
+                Console.WriteLine("Ugyldig indtastning. Indtast venligst et tal.");
+            }
+
+
+
+            Console.WriteLine("\nVil du lave en ny beregning? (ja/nej)");
+            string svar = Console.ReadLine();
+
+            if (svar.ToLower() == "ja" || svar.ToLower() == "j")
+            {
+                Start();
+
+            }
+
+
+            Console.WriteLine("\nFor at afslutte programmet, tryk på en tast");
+            Console.ReadKey();
+        }
+        private int Bro()
+        {
+            Console.WriteLine("Hvad for en bro har du taget (1/2/3)");
+            Console.WriteLine("(1) Ingen bro \n(2) Storebæltsbroen \n(3) Øresundsbroen");
+
+            int valg = int.Parse(Console.ReadLine());
+            if (valg == 0 || valg > 3)
+            {
+                Console.WriteLine("Fejl! Prøv igen");
+                Console.Clear();
+                Bro();
+            }
+            if (valg == 1) return 0;
+            else if (valg == 2) return 220;
+            else if (valg == 3) return 100;
+            else return 0;
+        }
+    }
+}
---
+++ kørselsfradrag/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using kørselsfradrag;
+
+
+
+namespace kørselsfradrag // Note: actual namespace depends on the project name.
+{
+    public class KilometerBeregner
+    {
+        private const double PrisOver24Km = 2.16;
+        private const int GrænseKilometer = 24;
+
+        private double antalKilometer;
+
+
+        public double AntalKilometer
+        {
+            get { return antalKilometer; }
+            set { antalKilometer = value; }
+        }
+
+        public double BeregnPris()
+        {
+            if (AntalKilometer > 120)
+            {
+
+                double over120 = AntalKilometer * 1.08;
+
+
+                double peng = ((120 - 24) * 2.19 + (AntalKilometer - 120) * 1.1) * 0.26;
+                double tillæg = (AntalKilometer - GrænseKilometer) * PrisOver24Km + over120;
+
+
+
+                return peng;
+            }
+            else if (AntalKilometer > GrænseKilometer)
+            {
+                double tillæg = (AntalKilometer - GrænseKilometer) * PrisOver24Km * 0.26;
+                return tillæg;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Beregner nyBer = new Beregner();
+            nyBer.Start();
+        }
+
+    }
+}
